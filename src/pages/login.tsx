e220ec4b461970af328a487c5bb44b1dca804881
kakimoty <kakimoty@gmail.com>
2021-09-30 22:14:20 +0900

    Fix for Index

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,9 +3,8 @@ import Button from '@mui/material/Button'
 import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-
-import { Layout } from '../components/Layout'
 import { SysConst } from '../lib/SysConst'
+import { Layout } from '../components/Layout'
 
 export default function Login() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
